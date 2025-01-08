54ce99b84002a1fb7434d7bc5ca5e4f360f220da
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-01-08 19:58:27 -0400

    fix(db): Correct file import path for config.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
-const config = require('./config.json');
+const config = require('./config/config.js');
 
 const environment = process.env.NODE_ENV || 'development';
 const dbConfig = config[environment];
