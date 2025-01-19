1ab20def90d6060f0c22c62403fccfe098ed7636
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-01-19 17:07:55 -0400

    feat(productService): add getAllProducts and search methods

--- services/productService.js.js
+++ services/productService.js.js
@@ -3,7 +3,7 @@ const { Product } = require('../models/product');
 class productService {
   async addProduct(productData) {
     return await Product.create(productData);
-  }
+  };
 
   async updatePrice(productId, newPrice) {
     const product = await Product.findByPk(productId);
@@ -13,7 +13,7 @@ class productService {
     product.price = newPrice;
     await product.save();
     return product;
-  }
+  };
 
   async updateInventory(productId, newStock) {
     const product = await Product.findByPk(productId);
@@ -23,7 +23,24 @@ class productService {
     product.stock = newStock;
     await product.save();
     return product;
-  }
+  };
+
+  async getAllProducts() {
+    const products = await Product.findAll();
+    return products;
+  };
+
+  async searchProducts(query) {
+    const products = await Product.findAll({
+      where: {
+        [Op.or]: [
+          { name: { [Op.like]: `%${query}%`}},
+          { description: { [Op.like]: `%${query}%`}}
+        ]
+      }
+    });
+    return products;
+  };
 }
 
 module.exports = new productService();
