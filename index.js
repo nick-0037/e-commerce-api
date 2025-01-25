5f3e7968861db8f00a9fd17ec7ecc67e76179401
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-01-25 19:53:06 -0400

    feat(index.js): add user validation, validation middleware, and global error handling

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const userController = require('./controllers/userController');
-const productController = require('./controllers/productController')
+const productController = require('./controllers/productController');
+const cartController = require('./controllers/cartController');
+const userValidation = require('./validations/userValidation');
+const validationMiddleware = require('./middlewares/validationMiddleware');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
@@ -9,15 +13,27 @@ app.use(express.json());
 
 const router = express.Router();
 
-router.get('/register', userController.register);
-router.post('/login', userController.login);
+router.post('/register', userValidation.register, validationMiddleware, userController.register);
+router.get('/login', userValidation.login, validationMiddleware, userController.login);
 
-router.post('/admin/products/:id', productController.addProduct);
-router.patch('/admin/products/:id/price', productController.updatePrice);
-router.patch('/admin/products/:id/inventory', productController.updateInventory);
+router.post('/products/:id', productController.addProduct);
+router.patch('/products/:id/price', productController.updatePrice);
+router.patch('/products/:id/inventory', productController.updateInventory);
 
 router.get('/products', productController.getAllProducts);
 router.get('/products/search', productController.searchProducts);
+
+router.post('/cart/:userId', cartController.addToCart);
+router.delete('/cart/:userId/:productId', cartController.removeFromCart);
+router.get('/cart/:userId/clear', cartController.clearCart);
+
+router.post('/cart/:userId/checkout', cartController.checkout);
+router.post('/cart/:userId/checkout/completed', cartController.completedCheckout);
+router.post('/cart/:userId/checkout/cancelled', cartController.cancelledCheckout);
+
+app.use('/api', router);
+
+app.use(errorHandler);
 
 const PORT = 3000;
 
