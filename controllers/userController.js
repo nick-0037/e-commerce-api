6f44685e5d03bfa59e2f22abe54ef44b115a9228
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-01-25 11:57:32 -0400

    refactor(userController): refactor code in user controller to improve logic flow

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,51 +1,44 @@
-const userService = require('../services/userService');
+const userService = require("../services/userService");
 
 class UserController {
-  
-  async register(req, res) {
+  async register(req, res, next) {
     try {
       const { email, username, password } = req.body;
-      const user = await userService.createUser({ email, username, password});
+      const { user, message } = await userService.createUser({
+        email,
+        username,
+        password,
+      });
       const token = userService.generateAuthToken(user);
-      
+
       res.status(201).json({
-        message: 'User registered successfully',
-        user: {
-          id: user.id,
-          username: user.username,
-          email: user.email
-        },
+        message,
+        user,
         token,
       });
     } catch (err) {
-      res.status(400).json({ message: err.message});
+      next(err);
     }
-  };
+  }
 
-  async login (req, res) {
+  async login(req, res, next) {
     try {
-      const { username, password } = req.body
-      const user = await userService.findUserByUsername(user);
-
-      if(!user || !(await User.validatePassword(password))) {
-        return res.status(400).json({ message: 'Invalid credentials' });
-      }
-
+      const { username, password } = req.body;
+      const { user, message } = await userService.loginUser({
+        username,
+        password,
+      });
       const token = userService.generateAuthToken(user);
 
       res.status(200).json({
-        message: 'Login successfully',
-        user: {
-          id: user.id,
-          username: username,
-          email: user.email
-        },
-        token
+        message,
+        user,
+        token,
       });
     } catch (err) {
-      res.status(400).json({ message: err.message });
+      next(err);
     }
-  };
-};
+  }
+}
 
-modules.exports = new UserController();
+module.exports = new UserController();
