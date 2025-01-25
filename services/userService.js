b83e74da515b5381afbf542cd52c6ae710c7d349
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-01-25 09:12:20 -0400

    refactor(userService): refactor code in user service for better readability and maintainability

--- services/userService.js
+++ services/userService.js
@@ -1,18 +1,48 @@
-const { User } = require('./models/User');
+const db = require('../models');
+const User = db.User;
 const jwt = require('jsonwebtoken');
 
 class UserService {
   async createUser(data) {
     const { email, username, password } = data;
+
     const user = await User.create({ email, username, password });
+    
+    return {
+      message: 'User registered successfully',
+      user
+    };
+  };
+  
+  async loginUser({ username, password }) {
+    const user = await UserService.findUserByUsername(username);
+    
+    const isValidPassword = await user.validatePassword(password)
+
+    if(!isValidPassword) {
+      const error = new Error('Invalid password');
+      error.statusCode = 401;
+      throw error;
+    }
+
+    return { 
+      message: 'Login successful',
+      user
+    };
+  };
+  
+  static async findUserByUsername(username) {
+    const user = await User.findOne({ where: { username }});
+
+    if (!user) {
+      const error = new Error('User not found');
+      error.statusCode = 404;
+      throw error;
+    };
+
     return user;
   };
-
-  async findUserByUsername(username) {
-    const user = await User.findOne({ where: { username }});
-    return user;
-  }
-
+  
   generateAuthToken(user) {
     const token = jwt.sign(
       { userId: user.id, email: user.email, role: user.role},
