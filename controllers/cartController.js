019c1a57fddcee2d1f1bc2358b1c66956bf69dc9
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-02-14 14:19:15 -0400

    feat(cartController): implement cart management methods including add, remove, clear, checkout, completedCheckout, and cancelledCheckout

---
+++ controllers/cartController.js
@@ -0,0 +1,88 @@
+const cartService = require("../services/cartService");
+
+class cartController {
+  async addToCart(req, res, next) {
+    try {
+      const { userId } = req.params;
+      const { productId, quantity } = req.body;
+      const item = await cartService.addToCart(userId, productId, quantity);
+
+      res.status(200).json({
+        message: "Product added to cart",
+        item,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async removeFromCart(req, res) {
+    try {
+      const { userId, productId } = req.params;
+      await cartService.removeFromCart(userId, productId);
+
+      res.status(200).json({
+        message: "Product removed from cart successfully",
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async clearCart(req, res) {
+    try {
+      const { userId } = req.params;
+      await cartService.clearCart(userId);
+
+      res.status(200).json({
+        message: "Cart cleared successfully",
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async checkout(req, res) {
+    try {
+      const { userId } = req.params;
+      const { message, paymentIntent } = await cartService.checkout(userId);
+
+      res.status(200).json({
+        message: "Checkout successfully",
+        paymentIntent,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async completedCheckout(req, res) {
+    try {
+      const { userId } = req.params;
+      const { cart } = await cartService.completedCheckout(userId);
+
+      res.status(200).json({
+        message: "Checkout completed successfully",
+        cart,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async cancelledCheckout(req, res) {
+    try {
+      const { userId } = req.params;
+      const { cart } = await cartService.cancelCart(userId);
+
+      res.status(200).json({
+        message: "Cart cancelled successfully",
+        cart,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+module.exports = new cartController();
