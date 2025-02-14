a87ea9b0cd1529db8fce7783fe5fb9860e209fd4
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-02-14 16:55:31 -0400

    refactor(migration): standardize table name and formatting in cart creation migration

--- migrations/20250114012943-create-cart.js
+++ migrations/20250114012943-create-cart.js
@@ -1,30 +1,30 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Carts', {
+    await queryInterface.createTable("carts", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       status: {
-        type: Sequelize.ENUM('pending', 'completed', 'cancelled'),
+        type: Sequelize.ENUM("pending", "completed", "cancelled"),
         allowNull: false,
-        defaultValue: 'pending'
+        defaultValue: "pending",
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Carts');
-  }
+    await queryInterface.dropTable("carts");
+  },
 };
