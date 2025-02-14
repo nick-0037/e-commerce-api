165eca8b9009b3955c25d7e39595662f5f04ba6a
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-02-14 14:12:05 -0400

    refactor(cartItemModel): standardize associations and formatting, add table name

--- models/cartitem.js
+++ models/cartitem.js
@@ -1,43 +1,48 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class CartItem extends Model {
     static associate(models) {
-      CartItem.belongsTo(models.Cart, { foreignKey: 'cartId' });
-      CartItem.belongsTo(models.Product, { foreignKey: 'productId' });
+      CartItem.belongsTo(models.Cart, { foreignKey: "cartId", as: "cart" });
+      CartItem.belongsTo(models.Product, {
+        foreignKey: "productId",
+        as: "product",
+      });
     }
   }
-  CartItem.init({
-    quantity: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 1
-    },
-    cartId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'carts',
-        key: 'id'
+  CartItem.init(
+    {
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
       },
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE'
-    },
-    productId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'products',
-        key: 'id'
+      cartId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Cart",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE'
+      productId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Product",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+    },
+    {
+      sequelize,
+      modelName: "CartItem",
+      tableName: "cart_items",
     }
-  }, {
-    sequelize,
-    modelName: 'CartItem',
-  });
+  );
   return CartItem;
 };
