97bd9c0c663ee23a7634edd2c1bae24b13d8531a
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-02-14 14:01:13 -0400

    refactor(productModel): improve code formatting, add description and table name, and standardize association

--- models/product.js
+++ models/product.js
@@ -1,43 +1,48 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Product extends Model {
     static associate(models) {
-      Product.belongsToMany(models.Cart, { through: 'CartItem', foreignKey: 'productId'});
+      Product.belongsToMany(models.Cart, {
+        through: "cartItems",
+        foreignKey: "productId",
+      });
     }
   }
-  Product.init({
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  Product.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          isDecimal: true,
+          min: 0,
+        },
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          isInt: true,
+          min: 0,
+        },
+      },
     },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        isDecimal: true,
-        min: 0
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        isInt: true,
-        min: 0
-      }
-    },
-  }, {
-    sequelize,
-    modelName: 'Product',
-  });
+    {
+      sequelize,
+      modelName: "Product",
+      tableName: "products",
+    }
+  );
   return Product;
 };
