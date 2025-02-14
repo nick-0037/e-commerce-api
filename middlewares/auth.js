064edfae080cc3baeff36c3611780a765fa0eed4
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-02-14 14:20:36 -0400

    fix(authMiddleware): improve token extraction and validation from authorization header

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization');
+  const authHeader = req.headers.authorization;
 
-  if (!token) {
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
     return res.status(401).json({ message: 'Access denied'});
   }
+
+  const token = authHeader.split(' ')[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
