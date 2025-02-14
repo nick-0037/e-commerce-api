84618270b8adf622359ba4e1c5b5a1923f1fcd32
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-02-14 14:13:39 -0400

    refactor(userService): standardize code formatting and improve readability

--- services/userService.js
+++ services/userService.js
@@ -1,57 +1,57 @@
-const db = require('../models');
+const db = require("../models");
 const User = db.User;
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 class UserService {
   async createUser(data) {
     const { email, username, password } = data;
 
-    const existingUser = await User.findOne({ where: { email }});
+    const existingUser = await User.findOne({ where: { email } });
     if (existingUser) {
-      const error = new Error('Email is already in use');
+      const error = new Error("Email is already in use");
       error.statusCode = 400;
       throw error;
     }
 
     const user = await User.create({ email, username, password });
-    
+
     return user;
-  };
-  
+  }
+
   async loginUser({ username, password }) {
     const user = await UserService.findUserByUsername(username);
-    
-    const isValidPassword = await user.validatePassword(password)
 
-    if(!isValidPassword) {
-      const error = new Error('Invalid password');
+    const isValidPassword = await user.validatePassword(password);
+
+    if (!isValidPassword) {
+      const error = new Error("Invalid password");
       error.statusCode = 401;
       throw error;
     }
 
     return user;
-  };
-  
+  }
+
   static async findUserByUsername(username) {
-    const user = await User.findOne({ where: { username }});
+    const user = await User.findOne({ where: { username } });
 
     if (!user) {
-      const error = new Error('User not found');
+      const error = new Error("User not found");
       error.statusCode = 404;
       throw error;
-    };
+    }
 
     return user;
-  };
-  
+  }
+
   generateAuthToken(user) {
     const token = jwt.sign(
-      { userId: user.id, email: user.email, role: user.role},
+      { userId: user.id, email: user.email, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: '1h' }
+      { expiresIn: "1h" }
     );
     return token;
-  };
-};
+  }
+}
 
 module.exports = new UserService();
