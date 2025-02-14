c16aa91b1f916f23077d6c17ffd1c4c64c045d53
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-02-14 14:18:22 -0400

    refactor(productController): standardize code formatting and improve readability

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,19 +1,18 @@
-const productService = require('../services/productService.js');
+const productService = require("../services/productService.js");
 
 class productController {
-  
   async addProduct(req, res, next) {
     try {
       const productData = req.body;
       const product = await productService.addProduct(productData);
-      res.status(201).json({ 
-        message: 'Product added successfully', 
-        product 
+      res.status(201).json({
+        message: "Product added successfully",
+        product,
       });
     } catch (err) {
-        next(err);
+      next(err);
     }
-  };
+  }
 
   async updatePrice(req, res, next) {
     try {
@@ -21,49 +20,52 @@ class productController {
       const productPrice = req.body.price;
 
       const product = await productService.updatePrice(productId, productPrice);
-      res.status(200).json({ 
-        message: 'Product price updated successfully',
-        product
+      res.status(200).json({
+        message: "Product price updated successfully",
+        product,
       });
     } catch (err) {
-        next(err);
+      next(err);
     }
-  };
+  }
 
   async updateInventory(req, res, next) {
     try {
       const productId = req.params.id;
       const productInventory = req.body.stock;
 
-      const product = await productService.updateInventory(productId, productInventory);
+      const product = await productService.updateInventory(
+        productId,
+        productInventory
+      );
       res.status(200).json({
-        message: 'Product inventory updated successfully',
-        product
-      })
+        message: "Product inventory updated successfully",
+        product,
+      });
     } catch (err) {
-        next(err);
+      next(err);
     }
-  };
+  }
 
   async getAllProducts(req, res, next) {
     try {
       const products = await productService.getAllProducts();
       res.status(200).json(products);
     } catch (err) {
-        next(err);
+      next(err);
     }
-  };
+  }
 
   async searchProducts(req, res, next) {
     try {
       const { q } = req.query;
       const products = await productService.searchProducts(q);
-      
+
       res.status(200).json(products);
     } catch (err) {
-        next(err);
+      next(err);
     }
   }
-};
+}
 
 module.exports = new productController();
