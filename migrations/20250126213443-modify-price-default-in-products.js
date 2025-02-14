4eda09bd33f328f2dc4910e2458ec2965e98539f
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-02-14 16:56:53 -0400

    refactor(migration): modify default value for price column in products table

--- migrations/20250126213443-modify-price-default-in-products.js
+++ migrations/20250126213443-modify-price-default-in-products.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('Products', 'price', {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn("products", "price", {
       type: Sequelize.DECIMAL(10, 2),
       allowNull: false,
-      defaultValue: 0 
+      defaultValue: 0,
     });
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('Products', 'price', {
+  async down(queryInterface, Sequelize) {
+    await queryInterface.changeColumn("products", "price", {
       type: Sequelize.DECIMAL(10, 2),
-      allowNull: false
+      allowNull: false,
     });
-  }
+  },
 };
