79448c1cd440eff629c11e5115f3ecc453cbdb75
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2025-02-15 10:03:03 -0400

    refactor(config): add port configuration and ensure consistent formatting

--- config/config.js
+++ config/config.js
@@ -1,16 +1,17 @@
 module.exports = {
   development: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS, 
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    port: process.env.DB_PORT,
+    dialect: "mysql",
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql'
-  }
-}
+    dialect: "mysql",
+  },
+};
